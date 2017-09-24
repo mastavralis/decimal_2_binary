b51e69721fe8ea7bd3ce48c1cfb8a69176e19bd9
Dionisis Mastavralis <5062626+mastavralis@users.noreply.github.com>
2017-09-24 13:31:24 +0300

    Update decimal_2_binary.py

--- decimal_2_binary.py
+++ decimal_2_binary.py
@@ -11,7 +11,7 @@ as input and converts it into binary number.
 binary = []
 
 # convert any decimal number to binary
-def decToBinary(n):
+def dec_2_Binary(n):
     
     if(n == 0): # this is the base case
         # we are done
@@ -21,8 +21,8 @@ def decToBinary(n):
         m = n % 2 # find the remainder between n % 2
         x = n / 2 # divide n by two to continue the sequence
         binary.append(m) # append the result of the division in to the list
-        decToBinary(x) # recall function recursively
+        dec_2_Binary(x) # recall function recursively
     
 
 # call the function with any decimal number as input
-decToBinary(88)
+dec_2_Binary(88)
